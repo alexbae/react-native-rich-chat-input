346b032f15532d78105133e76206bbbdd9a152ac
junmanbae <alexbae84@gmail.com>
2026-03-24 12:10:38 -0700

    fix(ios): disable scroll bounce when content fits within input bounds (#7)
    * fix(ios): disable scroll bounce when content fits within input bounds
    
    * fix: skip bob build on npm install (lib is pre-committed)
    
    * chore: update yarn.lock after prepare script change
    
    * style: fix prettier formatting in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export { RichChatInput } from './RichChatInput';
-export type { RichChatInputProps, RichContentResult, ContentSizeResult } from './RichChatInput';
+export type {
+  RichChatInputProps,
+  RichContentResult,
+  ContentSizeResult,
+} from './RichChatInput';
 export { default as RichChatInputView } from './RichChatInputViewNativeComponent';
